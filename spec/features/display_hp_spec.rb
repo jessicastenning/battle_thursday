0b04abe2fec2f1ef3b13688a7d7afd2fd7c32c99
Robert <fishwickrob@live.com>
2018-05-02 15:47:36 +0100

    test helpers

--- spec/features/display_hp_spec.rb
+++ spec/features/display_hp_spec.rb
@@ -1,9 +1,6 @@
 feature 'HP display' do
   scenario 'visibility of HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Dave: 60HP, Mittens: 60HP"
   end
 end
